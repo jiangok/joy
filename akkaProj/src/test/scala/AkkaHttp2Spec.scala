5f3b3007bb6f36375d663354f15cb3e3c6df3552
Lian Jiang <lian@sumologic.com>
2015-07-23 21:12:55 -0700

    add more akka http post test

--- akkaProj/src/test/scala/AkkaHttp2Spec.scala
+++ akkaProj/src/test/scala/AkkaHttp2Spec.scala
@@ -6,8 +6,9 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.ContentTypes._
 
-
 class Service2Spec extends FlatSpec with Matchers with ScalatestRouteTest {
+
+  val map = Map("red" -> 1, "yellow" -> 2)
 
   val routes = {
     (get & path("a1" / IntNumber / "edf")) { s =>
@@ -25,8 +26,20 @@ class Service2Spec extends FlatSpec with Matchers with ScalatestRouteTest {
           Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> s.toString }
         }
       } ~ (get & path("a4" / Segment / "ddd")) { s =>
-       complete {
-          Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> s }
+      complete {
+        Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> s }
+      }
+    } ~ (get & path("a5" / map)) { s =>
+      complete {
+        Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> s.toString }
+      }
+    } ~ (get & path("a6" / map / map)) { (firstMatch, secondMatch)  =>
+      complete {
+        Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> secondMatch.toString }
+      }
+    } ~ (post & path("a7") & entity(as[String])) { s =>
+      complete {
+        Future[Unit] {}.map[ToResponseMarshallable] { case _ => OK -> s }
       }
     }
   }
@@ -38,21 +51,26 @@ class Service2Spec extends FlatSpec with Matchers with ScalatestRouteTest {
   }
 
   Get("/a2/hello/abc") ~> routes ~> check {
-    status shouldBe OK
-    contentType shouldBe `text/plain(UTF-8)`
     responseAs[String] shouldBe "hello"
   }
 
   Get("/a3/ddd/hello") ~> routes ~> check {
-    status shouldBe OK
-    contentType shouldBe `text/plain(UTF-8)`
     responseAs[String] shouldBe "ddd/hello"
   }
 
   Get("/a4/hello/ddd") ~> routes ~> check {
-    status shouldBe OK
-    contentType shouldBe `text/plain(UTF-8)`
     responseAs[String] shouldBe "hello"
   }
 
+  Get("/a5/yellow") ~> routes ~> check {
+    responseAs[String] shouldBe "2"
+  }
+
+  Get("/a6/yellow/red") ~> routes ~> check {
+    responseAs[String] shouldBe "1"
+  }
+
+  Post("/a7", "content") ~> routes ~> check {
+    responseAs[String] shouldBe "content"
+  }
 }
