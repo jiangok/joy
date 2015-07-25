a67976cbf5bd1738e2a46725252d833f0d7a3042
Lian Jiang <lian@sumologic.com>
2015-07-24 22:32:41 -0700

    akkaProj - marshall/unmarshall dog list

--- akkaProj/src/test/scala/AkkaHttp2Spec.scala
+++ akkaProj/src/test/scala/AkkaHttp2Spec.scala
@@ -7,12 +7,15 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.scaladsl.{Sink, Flow, Source}
 import org.scalatest._
+import spray.json.DefaultJsonProtocol
 import scala.concurrent.duration._
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Promise, Await, Future}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
-
+import scala.util.Try
+import scalaz._
+import Scalaz._
 
 class AkkaHttp2Spec
   extends FlatSpec with Matchers with ScalatestRouteTest with Protocols {
@@ -48,68 +51,34 @@ class AkkaHttp2Spec
   }
 }
 
-/*
+case class Dog (name : String)
+case class Dogs (dogs : List[Dog])
 
-http://10.141.141.10:8080/v2/apps
-{"apps":[
-   {"id":"/andrew",
-    "cmd":null,
-     "args":[],
-     "user":null,
-     "env":{},
-     "instances":0,
-     "cpus":0.2,
-     "mem":32.0,
-     "disk":0.0,
-     "executor":"",
-     "constraints":[],
-     "uris":[],
-     "storeUrls":[],
-     "ports":[10000,10001,10002],
-     "requirePorts":false,
-     "backoffSeconds":1,
-     "backoffFactor":1.15,
-     "maxLaunchDelaySeconds":3600,
-     "container":{
-         "type":"DOCKER",
-         "volumes":[],
-         "docker":{
-             "image":"jiangok/fatapp:0.1-SNAPSHOT",
-             "network":"BRIDGE",
-             "portMappings":[{
-                 "containerPort":5099,
-                 "hostPort":0,
-                 "servicePort":10000,
-                 "protocol":"tcp"},
-                 {"containerPort":22,
-                  "hostPort":0,
-                  "servicePort":10001,
-                  "protocol":"tcp"},
-                 {"containerPort":2480,
-                  "hostPort":0,
-                  "servicePort":10002,
-                  "protocol":"tcp"}],
-              "privileged":false,
-              "parameters":[],
-              "forcePullImage":false}},
-      "healthChecks":[],
-      "dependencies":[],
-      "upgradeStrategy":{
-          "minimumHealthCapacity":1.0,
-          "maximumOverCapacity":1.0},
-      "labels":{},
-      "version":"2015-07-19T15:38:53.816Z",
-      "tasksStaged":0,
-      "tasksRunning":2,
-      "tasksHealthy":0,
-      "tasksUnhealthy":0,
-      "deployments":[{"id":"d4ebc706-3214-4c2c-98b5-845ead2594eb"}]}]}
- */
+trait DogProtocol extends DefaultJsonProtocol {
+  implicit val dogFormat = jsonFormat1(Dog.apply)
+  implicit val dogsFormat = listFormat(dogFormat)
+}
 
-class MarathonApiSpec
-  extends FlatSpec with Matchers with ScalatestRouteTest
-{
-  "marathon" should "respond to apps query" in {
+class DogSpec
+  extends FlatSpec with Matchers with ScalatestRouteTest with DogProtocol {
 
+  "dogs" should "be serialized/deserialized" in {
+    val d1 = Dog("tom")
+    val d2 = Dog("mike")
+    val dogs = List(d1, d2)
+
+    val d1Entity = marshal(d1)
+    val dogsEntity = marshal(dogs)
+
+    val future1 = Unmarshal(d1Entity).to[Dog]
+    val future2 = Unmarshal(dogsEntity).to[List[Dog]]
+
+    Await.result(future1, 5 seconds)
+    Await.result(future2, 5 seconds)
+
+    assert(future1.value.get.get == d1)
+    assert(future2.value.get.get.head == d1)
+    assert(future2.value.get.get.last == d2)
   }
 }
+
